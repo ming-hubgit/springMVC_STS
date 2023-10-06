28b092c3d91a8212e4fdd71a1f7f31baf5caf89d
mink <use1017@naver.com>
2023-10-06 14:33:24 +0900

    Properties사용하기

---
+++ project/BookRentalPjt/src/main/java/com/office/library/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.office.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	public BCryptPasswordEncoder bCryPasswordEncoder() {
+		System.out.println("[SecurityConfig] bCryPasswordEncoder()");
+		
+		return new BCryptPasswordEncoder();
+	}
+
+}
